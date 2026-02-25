659b7f44ae140a6dd03906769f7c4513832b4690
Chukwu Sydney <chukwusydney061@gmail.com>
2026-02-25 15:33:12 +0100

    Add IUnitOfWork and UnitOfWork Pattern

---
+++ generic-repo-uow-pattern-api/Repository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using generic_repo_pattern_api.Repository;
+
+namespace generic_repo_uow_pattern_api.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
